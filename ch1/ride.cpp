adc784664a0d3323d6a7cbe67ef13951f473d93a
kirchhoff <twd123@qq.com>
2013-09-18 16:31:40 +0800

    first commit

---
+++ ch1/ride.cpp
@@ -0,0 +1,47 @@
+/*
+ID:twd30651 
+PROG:ride
+LANG:C++
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int func(char *s)
+{
+    int ans = 1;
+    while(*s)
+    {
+        ans*=*s-('A'-1);
+        ans%=47;
+        s++;
+    }
+    return ans;
+}
+
+int main()
+{
+    freopen("ride.in","r",stdin);
+    freopen("ride.out","w",stdout);
+    char c[7],g[7];
+    cin>>c>>g;
+    if(func(c)==func(g))
+        cout<<"GO"<<endl;
+    else
+        cout<<"STAY"<<endl;
+    return 0;
+}
+/*  
+Executing...
+Test 1: TEST OK [0.011 secs, 3372 KB]
+Test 2: TEST OK [0.011 secs, 3372 KB]
+Test 3: TEST OK [0.011 secs, 3372 KB]
+Test 4: TEST OK [0.011 secs, 3372 KB]
+Test 5: TEST OK [0.011 secs, 3372 KB]
+Test 6: TEST OK [0.011 secs, 3372 KB]
+Test 7: TEST OK [0.011 secs, 3372 KB]
+Test 8: TEST OK [0.000 secs, 3372 KB]
+Test 9: TEST OK [0.011 secs, 3372 KB]
+Test 10: TEST OK [0.011 secs, 3372 KB]
+
+All tests OK.
+*/
