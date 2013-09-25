af7c8a55f94301f3db04c13a2e80626485bb06da
kirchhoff <twd123@qq.com>
2013-09-25 21:22:39 +0800

    add friday.cpp

---
+++ ch1/friday.cpp
@@ -0,0 +1,15 @@
+/*
+  ID:twd30651
+  PROG:friday
+  LANG:C++
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+     freopen("friday.in", "r", stdin);
+     freopen("friday.out","w",stdout);
+     
+     return 0;
+}
