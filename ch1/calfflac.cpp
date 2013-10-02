d04ec8b4d568c5513978a4d9a3647f1468974189
kirchhoff <twd123@qq.com>
2013-10-02 16:11:54 +0800

    start calfflac.cpp

---
+++ ch1/calfflac.cpp
@@ -0,0 +1,14 @@
+/*
+  ID:twd30651
+  PROG:calfflac
+  LANG:C++
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+     freopen("calfflac.in","r",stdin);
+     
+     return 0;
+}
