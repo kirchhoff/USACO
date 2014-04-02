9d0a37e6d8b5e692ebdd08113e816c990839dd9f
kirchhoff <twd123@qq.com>
2014-04-02 16:46:22 +0800

    add section1.1

---
+++ ch1/section1.1/beads3.cpp
@@ -0,0 +1,16 @@
+/*
+  ID:twd30651
+  PROG:beads
+  LANG:C++
+*/
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(int argc,char *argv[])
+{
+     freopen("beads.in","r",stdin);
+     // freopen("beads.out","w",stdout);
+
+     return 0;
+}
